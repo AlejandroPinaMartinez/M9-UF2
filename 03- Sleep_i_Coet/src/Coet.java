721e33cc52cc76f97cf8b81d6ebf391066fd111c
alep <alpimarti10@gmail.com>
2025-01-15 19:32:01 +0100

    Exercici acabat, sense el ús del join

--- 03- Sleep_i_Coet/src/Coet.java
+++ 03- Sleep_i_Coet/src/Coet.java
@@ -15,53 +15,56 @@ public class Coet {
         }
     }
 
-    // métode para comprobar y establir la potencia als 4 motors
+    // mètode per comprovar i establir la potència als 4 motors
     public void passaAPotencia(int p) {
         System.out.println("Passant a potència " + p);
 
-        // establir la potencia dels 4 motors
+        // establir la potència dels 4 motors
         for (int i = 0; i < 4; i++) {
             motors[i].setPotencia(p);
         }
 
-        // Crear un fil per cada motor
+        // Crear un fil per a cada motor
         Thread[] threads = new Thread[4];
         for (int i = 0; i < 4; i++) {
             threads[i] = new Thread(motors[i]);
-            threads[i].start(); // Iniciar el hilo
+            threads[i].start(); // Iniciar el fil
         }
 
-        // Esperar a que acabin tots els fils
-        try {
+        // Espera activa fins que tots els fils acabin
+        boolean totsElsFilsAcabats;
+        do {
+            totsElsFilsAcabats = true;
             for (Thread thread : threads) {
-                thread.join();
+                if (thread.isAlive()) {
+                    totsElsFilsAcabats = false;
+                    break;
+                }
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        } while (!totsElsFilsAcabats);
     }
 
-    // metode per arrencar els motors
+    // mètode per arrencar els motors
     public void arranca() {
         for (int i = 0; i < 4; i++) {
             motors[i].setPotencia(motors[i].getPotenciaObjectiu());
         }
     }
 
-    // métode per llegir la potencia desde la consola i establirla en tots els motors
+    // mètode per llegir la potència des de la consola i establir-la en tots els motors
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Coet coet = new Coet();
 
-        // llegir i assignar les potencies continuament
+        // llegir i assignar les potències contínuament
         while (true) {
             System.out.print("Introdueix la potencia objectiu: ");
             int potencia = sc.nextInt();
 
-            // establir la potencia de tots els motors
+            // establir la potència de tots els motors
             coet.passaAPotencia(potencia);
 
-            // si la potencia es 0, per a tots els cicles
+            // si la potència és 0, sortir del bucle
             if (potencia == 0) {
                 break;
             }
